8a7f786c5a7e110f5cf600dac14dc3138350f3e3
Nazar Volynets <nazikvol55@gmail.com>
2022-10-21 19:25:57 +0300

    50/50 tasks

---
+++ HW1/38.rb
@@ -0,0 +1,7 @@
+p 'Дан целочисленный массив. Найти индекс первого максимального элемента.'
+p 'arr = [2, -5, 43, 233, 1, 32, -31]'
+p 'm = []'
+p '(1..arr.length - 2).each do |i|'
+p   'm << arr[i] if arr[i - 1] < arr[i] && arr[i] > arr[i + 1]'
+p 'end'
+p 'puts arr.index(m.last)'

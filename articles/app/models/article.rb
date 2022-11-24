21542bc23280e38245b311b6447af1040900a59a
Nazar Volynets <nazikvol55@gmail.com>
2022-11-24 22:20:10 +0200

    Revert "This commit will be reverted"
    This reverts commit 01f7baffa010c39af106804411b9d6ee4bacbbab.

--- articles/app/models/article.rb
+++ articles/app/models/article.rb
@@ -1,4 +1,2 @@
 class Article < ApplicationRecord
-  belongs_to :author
-  has_many :comments, dependent: :destroy
 end
--- articles/app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :article
-
-  enum :status, { unpublished: 0, published: 1 }
-end

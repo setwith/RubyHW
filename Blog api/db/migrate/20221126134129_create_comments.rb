a41c3405c2d27376485d8de511168b0d7e9e5db5
Nazar Volynets <nazikvol55@gmail.com>
2022-11-26 16:04:04 +0200

    Added the restriction of creating a comment without linking to the author and the article

--- Blog api/db/migrate/20221126134129_create_comments.rb
+++ Blog api/db/migrate/20221126134129_create_comments.rb
@@ -1,10 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :body
       t.integer :status, default: 0
-      t.references :author, foreign_key: true
-      t.references :article, foreign_key: true
+      t.references :author, foreign_key: true, null: false
+      t.references :article, foreign_key: true, null: false
       t.timestamps
     end
   end

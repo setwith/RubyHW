a53369f17c83dcb36968ee41398edb2e42fd29a9
Nazar Volynets <nazikvol55@gmail.com>
2022-11-28 14:01:42 +0200

    Modified Comments controller

--- Blog api/app/controllers/api/v1/comments_controller.rb
+++ Blog api/app/controllers/api/v1/comments_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :set_comment, only: %i[show update destroy]
+
   def index
     @comments = Comment.all
     render json: @comments
   end
 
   def show
-    @comment = Comment.find(params[:id])
     render json: @comment
   end
 
@@ -19,7 +20,6 @@ class Api::V1::CommentsController < ApplicationController
   end
 
   def update
-    @comment = Comment.find(params[:id])
     if @comment.update(comment_params)
       render json: @comment, status: :ok
     else
@@ -27,9 +27,21 @@ class Api::V1::CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    if @comment.destroy
+      head :no_content
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comment_params
-    params.require(:comment).permit(:body, :article_id).merge(author_id: current_author.id)
+    params.require(:comment).permit(:body, :article_id, :author_id).merge(author_id: current_author.id)
   end
 end

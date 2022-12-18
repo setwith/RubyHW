d24bf7dd53d87f2de6a939932ef97af3d5d58ce6
Nazar Volynets <nazikvol55@gmail.com>
2022-12-18 16:09:47 +0200

    Added Article swagger

--- HW9/config/routes.rb
+++ HW9/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
       root to: 'articles#index_all'
@@ -6,8 +8,7 @@ Rails.application.routes.draw do
         get :published_comments
         get :unpublished_comments
       end
-      resources :articles do
-      end
+      resources :articles
       resources :likes, only: %i[create destroy]
       resources :comments, only: %i[index create update_status update destroy] do
         patch :update_status

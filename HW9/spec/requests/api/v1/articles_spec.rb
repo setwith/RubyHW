ac3a325fc56430a17be773a926b26274905d6b54
Nazar Volynets <nazikvol55@gmail.com>
2022-12-19 21:20:59 +0200

    Modified Article swagger

--- HW9/spec/requests/api/v1/articles_spec.rb
+++ HW9/spec/requests/api/v1/articles_spec.rb
@@ -76,23 +76,6 @@ RSpec.describe 'api/v1/articles', type: :request do
       end
     end
 
-    patch('update article') do
-      tags 'Articles'
-
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
     put('update article') do
       tags 'Articles'
       consumes 'application/json'

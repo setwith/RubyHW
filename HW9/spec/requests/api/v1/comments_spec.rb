cb4984b1d60dd5d094bade49a8379413fe57ccb4
Nazar Volynets <nazikvol55@gmail.com>
2022-12-19 21:18:19 +0200

    Added Comment swagger

---
+++ HW9/spec/requests/api/v1/comments_spec.rb
@@ -0,0 +1,113 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/comments', type: :request do
+  path '/api/v1/comments' do
+    get('list comments') do
+      tags 'Comments'
+
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create comment') do
+      tags 'Comments'
+      consumes 'application/json'
+      parameter name: :comment, in: :body, schema: {
+        type: :object,
+        properties: {
+          body: { type: :string },
+          status: { type: :string },
+          author_id: { type: :integer },
+          article_id: { type: :integer }
+        },
+        required: %w[body status author_id article_id]
+      }
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/comments/{comment_id}/update_status' do
+    # You'll want to customize the parameter types...
+    parameter name: 'comment_id', in: :path, type: :string, description: 'comment_id'
+
+    patch('update_status comment') do
+      tags 'Comments'
+
+      response(200, 'successful') do
+        let(:comment_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/comments/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    put('update comment') do
+      tags 'Comments'
+      consumes 'application/json'
+      parameter name: :comment, in: :body, schema: {
+        type: :object,
+        properties: {
+          body: { type: :string }
+        },
+        required: %w[body]
+      }
+
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete comment') do
+      tags 'Comments'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end

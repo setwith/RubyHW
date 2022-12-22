e9a439ac2c296e5ebb161d03afa2a0bfe7fc8cba
Nazar Volynets <nazikvol55@gmail.com>
2022-12-22 17:47:00 +0200

    Added references to model

--- HW10/shopper/app/models/product.rb
+++ HW10/shopper/app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 end
--- HW10/shopper/db/migrate/20221222144032_create_products.rb
+++ HW10/shopper/db/migrate/20221222144032_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 10, scale: 2
       t.string :image
 
       t.timestamps

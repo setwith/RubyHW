0505f9e120309b9c6bb03c17f8333b98207cacaf
Nazar Volynets <nazikvol55@gmail.com>
2022-12-23 15:27:40 +0200

    Added validation for price

--- HW10/shopper/app/models/product.rb
+++ HW10/shopper/app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   has_many :product_categories
   has_many :categories, through: :product_categories

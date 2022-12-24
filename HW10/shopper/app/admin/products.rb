ff52cb8ea4d80137dfd808b498147aa0b17e13c3
Nazar Volynets <nazikvol55@gmail.com>
2022-12-24 13:34:21 +0200

    Added nested attributes for product in Active Admin

--- HW10/shopper/app/admin/products.rb
+++ HW10/shopper/app/admin/products.rb
@@ -4,7 +4,8 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :description, :price, :image
+  permit_params :name, :description, :price, :image,
+                product_categories_attributes: %i[id product_id category_id _destroy]
   #
   # or
   #
@@ -13,4 +14,18 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  form do |f|
+    f.inputs 'Products category' do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :image
+      f.has_many :product_categories, allow_destroy: true do |n_f|
+        n_f.input :category
+      end
+
+      f.actions
+    end
+  end
 end
--- HW10/shopper/app/models/product.rb
+++ HW10/shopper/app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
 
   has_many :product_categories
   has_many :categories, through: :product_categories
+
+  accepts_nested_attributes_for :product_categories, allow_destroy: true
 end

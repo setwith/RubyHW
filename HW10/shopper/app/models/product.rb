7c269010cbbb0bf5b42e173af4416188e7d07193
Nazar Volynets <nazikvol55@gmail.com>
2022-12-24 12:51:46 +0200

    Installed Active Admin

--- HW10/shopper/app/models/product.rb
+++ HW10/shopper/app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :product_categories
   has_many :categories, through: :product_categories

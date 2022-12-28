1828f754af6791bf9a15e3886e48d190f0c49a96
Nazar Volynets <nazikvol55@gmail.com>
2022-12-28 16:48:54 +0200

    Added seed

--- HW10/shopper/db/seeds.rb
+++ HW10/shopper/db/seeds.rb
@@ -5,4 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# Category
+Category.create(title: 'Laptops and computers')
+Category.create(title: 'Smartphones, TV and electronics')
+Category.create(title: 'Clothes, shoes and jewelry')
+
+# Product
+Product.create(name: 'Computer ARTLINE',
+               description: 'Gaming ASUS Edition X43 (X43v33) Ryzen 5 3600/RAM 16ГБ/HDD 1ТБ + SSD 240ГБ/GeForce RTX 3050 8ГБ/Wi-Fi', price: 898, image: '')
+Product.create(name: 'Laptop Acer Aspire 7', description: 'A715-42G-R8BL (NH.QDLEU.008) Charcoal Black', price: 978,
+               image: '')
+Product.create(name: 'Apple MacBook Air 13', description: 'M1 256GB 2020 (MGND3) Gold', price: 1021, image: '')
+Product.create(name: 'TV Samsung', description: 'UE50AU7100UXUA', price: 655, image: '')
+Product.create(name: 'Apple iPhone 13', description: '128GB Pink ', price: 766, image: '')
+Product.create(name: 'T-shirt COS', description: '0960679 S white', price: '28', image: '')
+Product.create(name: 'Jack Wolfskin down jacket', description: 'Crystal Palace Coat 1204131-5066', price: '344',
+               image: '')
+Product.create(name: 'Pendant with Swarovski crystals', description: '9299 Italina', price: '98', image: '')
+
+# Product_categories
+ProductCategory.create(product_id: 1, category_id: 1)
+ProductCategory.create(product_id: 1, category_id: 2)
+ProductCategory.create(product_id: 2, category_id: 1)
+ProductCategory.create(product_id: 2, category_id: 2)
+ProductCategory.create(product_id: 3, category_id: 1)
+ProductCategory.create(product_id: 3, category_id: 2)
+ProductCategory.create(product_id: 4, category_id: 2)
+ProductCategory.create(product_id: 5, category_id: 2)
+ProductCategory.create(product_id: 6, category_id: 3)
+ProductCategory.create(product_id: 7, category_id: 3)
+ProductCategory.create(product_id: 8, category_id: 3)
+
+# User
+User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end

6b8a3b2b46eadb3f8bcccada1b6db8183c522cea
Nazar Volynets <nazikvol55@gmail.com>
2023-01-03 13:45:55 +0200

    Fixed delete bug, added depenent to product model

--- HW10/shopper/app/models/product.rb
+++ HW10/shopper/app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  has_many :product_categories
+  has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
   accepts_nested_attributes_for :product_categories, allow_destroy: true

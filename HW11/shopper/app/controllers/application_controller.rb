0b81d191a7ca2147c801d2d036c85fd616ec6db5
Nazar Volynets <nazikvol55@gmail.com>
2023-01-05 17:07:42 +0200

    Modified routes and create a session cart

--- HW11/shopper/app/controllers/application_controller.rb
+++ HW11/shopper/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def current_cart
+    Cart.find(cookies[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    cookies[:cart_id] = cart.id
+    cart
+  end
 end
--- HW11/shopper/config/routes.rb
+++ HW11/shopper/config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'products#index'
+  get 'cart', to: 'carts#show', as: :cart
 end

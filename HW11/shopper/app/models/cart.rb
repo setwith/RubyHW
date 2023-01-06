255bf1ef236645f0d171f966dbe900fcedf6717d
Nazar Volynets <nazikvol55@gmail.com>
2023-01-06 17:31:55 +0200

    Modified page for the cart

--- HW11/shopper/app/models/cart.rb
+++ HW11/shopper/app/models/cart.rb
@@ -4,4 +4,8 @@ class Cart < ApplicationRecord
   def add_product(product)
     line_items.create!(product:, quantity: 1)
   end
+
+  def total_price
+    line_items.map(&:product).sum(&:price)
+  end
 end

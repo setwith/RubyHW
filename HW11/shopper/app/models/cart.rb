5ebcc41bf79adcf762099d6e6b28a28853274edd
Nazar Volynets <nazikvol55@gmail.com>
2023-01-07 14:33:52 +0200

    Created an Order model with relations

--- HW11/shopper/app/models/cart.rb
+++ HW11/shopper/app/models/cart.rb
@@ -1,11 +1,10 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :nullify
+  has_one :order
 
   def add_product(product)
     line_items.create!(product:, quantity: 1)
   end
-
-
 
   def total_price
     line_items.to_a.sum { |line_item| line_item.total }
--- HW11/shopper/db/schema.rb
+++ HW11/shopper/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_144257) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_07_122600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_144257) do
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "user_id", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "product_categories", force: :cascade do |t|
     t.bigint "product_id", null: false
     t.bigint "category_id", null: false
@@ -94,6 +104,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_144257) do
 
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "products"
+  add_foreign_key "orders", "carts"
+  add_foreign_key "orders", "users"
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
 end

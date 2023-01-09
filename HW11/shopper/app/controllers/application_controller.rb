d4ea29c29c86c21552c17406c98022629264f627
Nazar Volynets <nazikvol55@gmail.com>
2023-01-09 20:14:20 +0200

    Added ransack controller

--- HW11/shopper/app/controllers/application_controller.rb
+++ HW11/shopper/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_querry
+
+  def set_querry
+    @querry = Product.ransack(params[:q])
+  end
+
   def current_cart
     Cart.find(cookies[:cart_id])
   rescue ActiveRecord::RecordNotFound

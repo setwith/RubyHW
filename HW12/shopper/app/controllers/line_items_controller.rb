f0f54d9c09a23728187b25357d02638df4cebc10
Nazar Volynets <nazikvol55@gmail.com>
2023-01-17 12:34:09 +0200

    Fixed line_item controller

--- HW12/shopper/app/controllers/line_items_controller.rb
+++ HW12/shopper/app/controllers/line_items_controller.rb
@@ -1,21 +1,22 @@
 class LineItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_line_item, only: %i[destroy update]
 
   def create
     product = Product.find(params[:product_id])
     @line_item = current_cart.line_items.find_by(product:)
 
-    if @line_item
+    if @line_item.present?
       @line_item.update(quantity: @line_item.quantity + 1)
     else
       @line_item = current_cart.add_product(product)
-
     end
-    redirect_to cart_path, notice: "#{product.name} was successfully added to cart"
+
+    redirect_to cart_path, notice: "#{product.name} was successfully added to the cart"
   end
 
   def update
-    quantity = params[:action] == 'increase' ? @line_item.quantity + 1 : @line_item.quantity - 1
+    quantity = params[:quantity] == 'increase' ? @line_item.quantity + 1 : @line_item.quantity - 1
     @line_item.update(quantity:)
 
     redirect_to cart_path
@@ -23,7 +24,8 @@ class LineItemsController < ApplicationController
 
   def destroy
     @line_item.destroy
-    redirect_to cart_path, notice: "#{@line_item.product.name} deleted from cart"
+
+    redirect_to cart_path(current_cart)
   end
 
   private

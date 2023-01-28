99e4e9209f9dc20812ab40f2256e11be41d84193
Nazar Volynets <nazikvol55@gmail.com>
2023-01-28 20:51:12 +0200

    Fixed product model and added action to Buy button

--- HW13/shopper/app/models/product.rb
+++ HW13/shopper/app/models/product.rb
@@ -11,7 +11,7 @@
 #
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_or_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
